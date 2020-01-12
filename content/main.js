78e86dc0fef0e6e397d6ecf08173391c32be3fa3
reactdev <reactdev@kakao.com>
2020-01-12 15:15:13 +0900

    toggle editor 버튼 눌러서 켜고 끌 수 있게 했어요 이젠 헷갈릴 일 없긔

--- content/main.js
+++ content/main.js
@@ -2,25 +2,22 @@ const editorElement = document.createElement("textarea");
 const editorStyle = document.createElement("style");
 editorStyle.append(
     document.createTextNode(`
-    @import url("//cdn.jsdelivr.net/gh/wan2land/d2coding/d2coding-ligature-full.css");
+@import url("//cdn.jsdelivr.net/gh/wan2land/d2coding/d2coding-ligature-full.css");
 
-    .ace_editor {
+.ace_editor {
     height: 200px;
-  }
-  iframe#frame_source {
-    display: none !important;
-  }`)
+    display: none;
+}`)
 );
-
-const sourceElement = document.getElementById("source");
-sourceElement.after(editorElement);
+const rawEditor = document.getElementById("source");
+document.querySelector('#language+br').after(editorElement);
 document.body.append(editorStyle);
 const editor = ace.edit(editorElement);
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/c_cpp");
 editor
     .getSession()
-    .on("change", () => (sourceElement.value = editor.getValue()));
+    .on("change", () => (rawEditor.value = editor.getValue()));
 editor.setOptions({
     fontFamily: "D2Coding ligature",
     fontSize: "18px"
@@ -35,9 +32,19 @@ document.addEventListener("keyup", ({ key, ctrlKey }) => {
 const loop = setInterval(() => {
     const toggle = document.getElementById("edit_area_toggle_checkbox_source");
     if (!toggle) return;
+    let isLegacyEditor = true;
+    const aceEditor = document.getElementsByClassName("ace_editor")[0];
+    const legacyEditor = document.getElementById("frame_source");
     clearInterval(loop);
-    toggle.addEventListener(
-        "click",
-        () => (sourceElement.style.display = "none")
-    );
+    toggle.addEventListener("click", e => {
+        isLegacyEditor = !isLegacyEditor;
+        rawEditor.style.display = 'none'
+        if (isLegacyEditor) {
+            legacyEditor.style.display = "block";
+            aceEditor.style.display = "none";
+            return;
+        }
+        legacyEditor.style.display = "none";
+        aceEditor.style.display = "block";
+    });
 }, 100);

59b15f6adc567df7f3559da0a4483f55a8a19a6f
jeonghan <reactdev@kakao.com>
2020-01-19 21:01:56 +0900

    ts적용

--- dist/main.js
+++ dist/main.js
@@ -259,7 +259,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53667" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62835" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
